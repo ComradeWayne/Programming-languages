382ba532d296663584080d14a67071d8f532a0c7
ComradeWayne <56508056+ComradeWayne@users.noreply.github.com>
2020-04-13 23:32:01 +0700

    1 Лабораторная работа. 5 вариант

---
+++ Lab1_Var5/ConsoleApplication1.2lab/ConsoleApplication1.2lab/ConsoleApplication1.2lab.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+#include "stdio.h"
+using namespace std;
+
+float* delfun(float* mas, int k, int n)
+{
+	float* mas2 = new float[n - 1]; // до того как хотим удалить кпирум до того как хотим удалить
+	for (int i=0; i < k; i++)
+		mas2[i] = mas[i];
+	for (int i = k; i < n - 1; i++) // копируем со сдвигом 
+		mas2[i] = mas[i + 1];
+	return mas2;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	float a, b;
+	int k;
+	cout << "Введите количество чисел" << endl;
+	cin >> n;
+	cout << "\nВведите границу интервала" << endl;
+	cin >> a >> b;
+	cout << "\nВведите k" << endl; // как фильтруется граница после все удаляем
+	cin >> k;
+	float* mas = new float[n];
+	for (int i = 0; i < n; i++)
+		mas[i] = a + rand() * (b - a) / RAND_MAX; // нормировали интервал
+	cout << "\nБыло" << endl;
+	for (int i = 0; i < n; i++)
+		cout << mas[i] << " ";
+	for (int i = 0; i < n; i++)
+	{
+		if ((mas[i]) > k) // если элемент больше заданного к, 
+		{
+			mas = delfun(mas, i, n);
+			delfun(mas, i, n); //удаляющая функция
+			n--; // коррекция длины массива
+			i--; // проверка нового элемента
+
+		}
+	}
+	cout << "\nСтало" << endl;
+	for (int i = 0; i < n; i++)
+		cout << mas[i] << " ";
+
+}
+	
---
+++ Lab1_Var5/ConsoleApplication1Lab/ConsoleApplication1Lab/ConsoleApplication1Lab.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <fstream>
+#include "stdio.h"
+
+using namespace std;
+
+int* func(int* mas, int n, int a)
+{
+	int* mas2 = new int[n + 1];
+	for (int i = 0; i < n; i++)
+		mas2[i] = mas[i];
+	mas2[n] = a;
+	return mas2;
+}
+void sort(int* mas, int n)
+{
+	int temp;
+	for (int i = 0; i < n - 1; i++)
+		for (int j = 0; j < n - 1; j++)
+		{
+			if (mas[j] > mas[j + 1])
+			{
+				// меняем элементы местами
+				temp = mas[j];
+				mas[j] = mas[j + 1];
+				mas[j + 1] = temp;
+			}
+		}
+}
+
+int main()
+{
+	int a;
+	int n = 0;
+	int* arr = new int[0];
+	ifstream file;
+	file.open("тестовый массив.txt");
+	if (!file.is_open())
+	{
+		printf("error");
+		return -1;
+	}
+	while (!file.eof())
+	{
+		file >> a;
+		if (file.eof())
+			exit;
+		arr = func(arr, n, a);
+		n++;
+	}
+	sort(arr, n);
+	file.close();
+	for (int i = 0; i < n; i++)
+		printf("%d \n", arr[i]);
+	scanf_s("%d", &n);
+	return 0;
+}
