7ec9e5daae7f0d17a6a6aedc2c2b8f43e0f4a307
ComradeWayne <56508056+ComradeWayne@users.noreply.github.com>
2020-04-20 02:10:02 +0700

    4 номер. 1 и 2 задачи

---
+++ ConsoleApplication4.2/ConsoleApplication1.2/ConsoleApplication1.2.cpp
@@ -0,0 +1,77 @@
+﻿
+#include <stdlib.h>
+#include <fstream>
+#include "stdio.h"
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	fstream file;
+	file.open("file.txt");
+	if (!file.is_open())
+	{
+		printf("error");
+		return -1;
+	}
+	int n, m;
+	file >> n;
+	file >> m;
+	int** a = new int* [m];
+	for (int i = 0; i < m; i++)
+		a[i] = new int[m];
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < m; j++)
+			file >> a[i][j];
+	int** c = new int* [n];
+	for (int i = 0; i < n; i++)
+		c[i] = new int[m];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			file >> c[i][j];
+	int* b = new int[m];
+	for (int i = 0; i < m; i++)
+		file >> b[i];
+	int* d = new int[n];
+	for (int i = 0; i < n; i++)
+		file >> d[i];
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < m; j++)
+		{
+			int d1;
+			a[i][j] = 5 * a[i][j];
+		}
+	int** res = new int* [m];
+	for (int i = 0; i < m; i++)
+		res[i] = new int[m];
+	int** res1 = new int* [m];
+	for (int i = 0; i < m; i++)
+		res1[i] = new int[n];
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			res1[i][j] = b[i] * d[j];
+		}
+	}
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			res[i][j] = 0;
+			for (int l = 0; l < n; l++)
+				res[i][j] += res1[i][l] * c[l][j];
+		}
+	}
+	cout << "Полученная матрица " << endl;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < m; j++)
+			cout << res[i][j] << " ";
+		cout << endl;
+	}
+	return 0;
+}
---
+++ ConsoleApplicationZadacha4.1.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <fstream>
+#include "stdio.h"
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, a, b;
+	cout << "Введите размер матрицы " << endl;
+	cin >> n;
+	cout << "Введите a и b" << endl;
+	cin >> a >> b;
+	int** matrix = new int* [n]; // матрица н строчек. две звезды указывают указывают на массив из игрок. одна создает ячейку игрока
+	for (int i = 0; i < n; i++) // для каждой ячейки массив результатов
+		matrix[i] = new int[n];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			matrix[i][j] = a + round(rand() * 1.0f * (b - a) / RAND_MAX); // 1.0f
+	int green, red;
+	int indgreen1, indgreen2, indred1, indred2;
+	red = b + 1;
+	green = a - 1; // заведомо меньше
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+		{
+			if (i<j && i + j > n-1) //green
+			{
+				if (matrix[i][j] < 0 && matrix[i][j]> green)
+				{
+
+					green = matrix[i][j];
+					indgreen1 = i;
+					indgreen2 = j;
+				}
+			}
+			if (i > j&& i + j > n-1)
+			{
+				if (matrix[i][j] > 0 && matrix[i][j] < red)
+				{
+					red = matrix[i][j];
+					indred1 = i;
+					indred2 = j;
+				}
+			}
+		}
+	if (red == b+1)
+		cout << "В красной области положительных элементов нет";
+	else if (green == a-1) 
+		cout << "В зеленой области отрицательных элементов нет";
+	else
+	{
+		cout << "Исходная матрица " << endl;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+				cout << matrix[i][j] << " ";
+			cout << endl;
+		}
+		cout << "max отрицательный " << green << endl;
+		cout << "min положительный " << red << endl;
+		int t = matrix[indred1][indred2];
+		matrix[indred1][indred2] = matrix[indgreen1][indgreen2];
+			matrix[indgreen1][indgreen2] = t;
+			cout << "Полученная матрица " << endl;
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+					cout << matrix[i][j] << " ";
+				cout << endl;
+			}
+	}
+	return 0;
+}
+
