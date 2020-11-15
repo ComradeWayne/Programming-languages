2d6228cbcab7fbfda55dd573e0c5766143e7f8c4
ComradeWayne <56508056+ComradeWayne@users.noreply.github.com>
2020-11-15 15:27:13 +0700

    Add files via upload

---
+++ Второй курс/3LabShablon/Mnozhestva/Mnozhestva/Mnozhestva.cpp
@@ -0,0 +1,230 @@
+﻿#include<iostream>
+#include "fstream"
+#include "stdlib.h"
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+template <class T>
+class Set
+{
+	int count; // количество 
+	T* numbers; // указатель на список из чисел
+public:
+	Set() // конструктор
+	{
+		count = 0; // нет элементов в созданном множестве
+		numbers = NULL; // указатель на список пуст
+	}
+	void Add(T n) // вставка
+	{
+		if (numbers == NULL) // если ранее список пуст
+		{
+			numbers = new T[1]; // создаём
+			numbers[0] = n; 
+			count++;
+		}
+		else // если список не пуст, проверем сколько было до этого элементов
+		{
+			bool f = true; // флажок
+			for (int i = 0; i < count; i++)
+				if (numbers[i] == n)
+					f = false;
+			if (f)
+			{
+				T* h = new T[count + 1]; // вспомогательная переменная. Массив на 1 больше чем был раньше
+				for (int i = 0; i < count; i++) // заполняем массив элементами, которые были до этого
+					h[i] = numbers[i]; 
+				h[count] = n; // заполняем новым числом, которое к нам пришло
+				delete[] numbers; // старый массив удаляем
+				numbers = h; // присваиваем новый массив
+				count++;
+			}
+		}
+		
+	}
+	bool def(int z) // метод определения четности
+	{
+		if (z % 2 == 1)
+			return true;
+		else return false;
+	}
+	void print() // метод. печатает все, что получилось
+	{
+		for (int i = 0; i < count; i++)
+			cout << numbers[i] << " ";
+		cout << endl;
+	}
+	int get_count() // метод, который выводит сколько элементов есть в множестве
+	{
+		return count;
+	}
+	void del(T k) // удаление элемента
+	{
+		int i = 0;
+		bool f = true;
+		while (i < (count) && f) 
+		{
+			if (numbers[i] == k)
+				f = false;
+			else i++;
+		}
+		
+		if (i < count)
+		{
+			for (int j = i; j < count - 1; j++)
+				numbers[j] = numbers[j + 1];
+			count--;
+			T* h = new T[count];
+			for (int j = 0; j < count; j++)
+				h[j] = numbers[j];
+			delete[] numbers;
+			numbers = h;
+		}
+	}
+	T get_elem(int i)
+	{
+		return numbers[i];
+	}
+	Set <T> union1(Set <T> M) // метод объединения
+	{
+		Set <T> S;
+		for (int i = 0; i < count; i++)
+			S.Add(numbers[i]);
+		for (int i = 0; i < M.get_count(); i++)
+			S.Add(M.get_elem(i));
+		return S;
+	}
+	Set <T> intersect(Set <T> M) // метод пересечения
+	{
+		Set <T> S;
+		for (int i = 0; i < count; i++)
+		{
+			bool t = false;
+			for (int j = 0; j < M.get_count(); j++)
+				if (numbers[i] == (M.get_elem(j)))
+					t = true;
+			if (t)
+				S.Add(numbers[i]);
+		}
+		return S;
+	}
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int d;
+	fstream file1;
+	file1.open("M1.txt");
+	if (!file1.is_open())
+	{
+		cout << "error";
+		return -1;
+	}
+	Set <int> M1;
+	while (!file1.eof())
+	{
+		file1 >> d;
+		if (M1.def(d))
+			M1.Add(d);
+	}
+	fstream file2;
+	file2.open("M2.txt");
+	if (!file2.is_open())
+	{
+		cout << "error";
+		return -1;
+	}
+	Set <int> M2;
+	while (!file2.eof())
+	{
+		file2 >> d;
+		if (M2.def(d))
+			M2.Add(d);
+	}
+	fstream file3;
+	file3.open("M3.txt");
+	if (!file3.is_open())
+	{
+		cout << "error";
+		return -1;
+	}
+	Set <int> M3;
+	while (!file3.eof())
+	{
+		file3 >> d;
+		if (M3.def(d))
+			M3.Add(d);
+	}
+	cout << "Первое множество М1" << endl;
+	M1.print();
+	cout << "Второе множество М2" << endl;
+	M2.print();
+	cout << "Третье множество М3" << endl;
+	M3.print();
+	cout << "1. Большее количество элементов в множестве " << endl;
+	int a = M1.get_count();
+	int b = M2.get_count();
+	int c = M3.get_count();
+	if ((a == b) && (a == c))
+		cout << "Во всех множествах одинаковое количество элементов: " << a << endl;
+	else if (a == b)
+	{
+		if (a > c)
+			cout << "Больше элементов в М1 и М2: " << a << endl;
+		else cout << "Больше элементов в М3: " << c << endl;
+	}
+	else if (a == c)
+	{
+		if (a > b)
+			cout << "Больше элементов в М1 и М3: " << a << endl;
+		else cout << "Больше элементов в М2: " << b << endl;
+	}
+	else if (b == c)
+	{
+		if (b > a)
+			cout << "Больше элементов в М2 и М3: " << b << endl;
+		else cout << "Больше элементов в М1: " << a << endl;
+	}
+	else
+	{
+		if ((a>b) && (a>c))
+			cout << "Больше элементов в М1: " << a << endl;
+		else if ((b > a) && (b > c))
+			cout << "Больше элементов в М2: " << b << endl;
+		else if ((c > a) && (c > b))
+			cout << "Больше элементов в М3: " << c << endl;
+	}
+	////
+	cout << "2. Пересечение множеств М1 и М2" << endl;
+	Set <int> M5 = M1.intersect(M2);
+	M5.print();
+	if (M5.get_count() == 0)
+		cout << "Эти множества не пересекаются" << endl;
+	if (M5.get_count() == M1.get_count())
+		cout << "Множество М1 вложено в М2" << endl;
+	else if (M5.get_count() == M2.get_count())
+		cout << "Множество М2 вложено в М1" << endl;
+	////
+	cout << "3. Добавить элемент в М1" << endl;
+	cout << "Введите число ";
+	int t;
+	cin >> t;
+	if (M1.def(t))
+		M1.Add(t);
+	cout << "Множество М1 после добавления нового элемента" << endl;
+	M1.print();
+	cout << "4. Удаление элемента из М2" << endl;
+	cout << "Введите число ";
+	cin >> t;
+	M2.del(t);
+	cout << "Множество М2 после возможного удаления элемента" << endl;
+	M2.print();
+	cout << "5. Объединение множеств М2 и М3" << endl;
+	Set <int> M4 = M2.union1(M3);
+	M4.print();
+	return 0;
+}
---
+++ Второй курс/3LabShablon/Shablon class/Shablon class/Shablon class.cpp
@@ -0,0 +1,307 @@
+﻿#include<iostream>
+#include "fstream"
+#include "stdlib.h"
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+class Complex // класс комплексных чисел
+{
+    double re, im; // целая и мнимая части
+
+    // создаем конструкторы
+public:
+
+    Complex() { re = 0; im = 0; };
+
+    Complex(double r) // конструктор по умолчанию
+    {
+        re = r;
+        im = 0;
+    }
+
+    Complex(double r, double i) // конструктор по умолчанию
+    {
+        re = r;
+        im = i;
+    }
+
+    Complex(Complex& c) // конструктор копирования
+    {
+        re = c.re;
+        im = c.im;
+    }
+
+    ~Complex() {}
+
+    float abs() // Модуль комплексного числа
+    {
+        return sqrt(re * re - im * im);
+    }
+
+    void set_(double r, double i)
+    {
+        re = r;
+        im = i;
+    }
+    Complex& operator = (Complex& c) // перегрузка оператора присваивания
+    {
+        re = c.re;
+        im = c.im;
+
+        return (*this);
+    }
+
+    Complex& operator + (Complex& c) // перегрузка оператора сложения
+    {
+
+
+        re = re + c.re;
+        im = im + c.re;
+
+        return *this;
+    }
+
+    Complex& operator - (Complex& c) // перегрузка оператора вычитания
+    {
+
+
+        re = re - c.re;
+        im = im - c.re;
+
+        return *this;
+    }
+
+    Complex& operator * (Complex& c) // перегрузка оператора умножения
+    {
+
+
+        re = re * c.re;
+        im = re * c.im;
+
+        return *this;
+    }
+
+    Complex& operator / (Complex& c) // перегрузка оператора деления
+    {
+
+
+        double r = c.re * c.re + c.im * c.im;
+        re = (re * c.re + im * c.im) / r;
+        re = (im * c.re - re * c.im) / r;
+
+        return *this;
+    }
+
+    friend ostream& operator<<(ostream&, const Complex&); // перегрузка оператора <<
+    friend istream& operator>>(istream&, const Complex&); // перегрузка оператора >>
+
+};
+
+ostream& operator<<(ostream& out, const Complex& c)
+{
+    out << c.re;
+    if (c.im >= 0)
+        out << "+";
+    else
+        out << "-";
+    out << c.im << "i";
+
+    return out;
+}
+
+istream& operator>>(istream& in, const Complex& c)
+{
+    in >> c.re >> c.im;
+
+    return in;
+}
+
+
+
+//template <typename T> Можно и так
+template <class T>
+class Vector
+{
+private:
+    int n;                 //размер вектора
+    T* coords;            //указатель на массив с координатами вектора
+
+public:
+    Vector(int k = 3) { coords = new T[k]; n = k; };           //Конструктор и деструктор
+    ~Vector() { cout << "destructor  " << endl; delete[] coords; coords = nullptr; };
+    Vector(const Vector& b);// конструктор копирования
+//const означает, что объект передается по ссылке, но изменять его нельзя
+    Vector& operator = (const Vector& b);// оператор присваивания
+    void Set_n(int k);             //Метод задания размера вектора
+    void Set_coords(T value, int i); //Метод для задания жлемента вектора
+    int Get_n(); // метод получения размера вектора
+
+    T Get_coords(int i) const; // метод получения элемента вектора
+// добавила спецификатор const , т. к. метод используется в операторе +
+// а параметр, для которого эта функция используется определен как const
+    double Get_size();
+
+    Vector operator+(Vector&); //перегрузка оператора сложения без создания локальной копии объекта с передачей по ссылке
+    T operator *(Vector&);
+};
+
+template <class T> Vector<T>::Vector(const Vector& b)
+{
+    n = b.n;
+    coords = new T[n]; // выделяем необходимый объем памяти
+    for (int i = 0; i < n; i++) coords[i] = b.coords[i]; // копируем поэлементно
+}
+
+template <class T> Vector<T>& Vector<T>::operator = (const Vector<T>& b)
+{
+    n = b.n;
+    delete[] coords; // освобождаем память которая была выделена раньше
+    coords = new T[n];// выделяем необходимый объем памяти
+    for (int i = 0; i < n; i++)
+        coords[i] = b.coords[i];   // копируем поэлементно
+    return *this;
+}
+
+template <class T> T Vector<T>::operator *(Vector& b)
+{
+    int n = b.n;
+    T s = 0;
+
+    for (int i = 0; i < n; i++)
+        s += coords[i] * b.coords[i];
+    return s;
+}
+
+
+template <class T> double Vector<T>::Get_size()
+{
+    //Complex z;
+    //z.set_(Get_n(), 0);
+    //return z;
+    return Get_n();
+}
+
+template<> double Vector<double>::Get_size()
+{
+    double s = 0;
+
+    for (int i = 0; i < n; i++)
+        s += coords[i] * coords[i];
+    return sqrt(s);
+
+};
+
+template<> double Vector<int>::Get_size()
+{
+    double s = 0;
+
+    for (int i = 0; i < n; i++)
+        s += coords[i] * coords[i];
+    return sqrt(s);
+
+};
+
+template <class T> Vector<T> Vector<T>:: operator+ (Vector<T>& b)
+{
+    int k = b.n;
+    //int k=b.Get_n();
+
+
+    Vector temp(k);
+    //  Вам необходимо добавить проверку размерности массивов - они должны быть одинаковой длины
+
+    for (int i = 0; i < k; i++)
+        temp.coords[i] = coords[i] + b.coords[i];
+    return temp;
+}
+
+template <class T> void Vector<T>::Set_n(int k)
+{
+    n = k;
+}
+template <class T> void Vector<T>::Set_coords(T value, int i)
+{
+    coords[i] = value;
+}
+
+template <class T> int Vector<T>::Get_n()
+{
+    return n;
+}
+template <class T> T Vector<T>::Get_coords(int i) const
+{
+    return coords[i];
+}
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double d;
+    int n;
+    int i;
+    int m;
+    
+    cout << "Демонстрация работы с типом int" << endl;
+    cout << "Введите размерность целочисленного вектора" << endl;
+    cin >> n;
+    cout << "Введите координаты вектора" << endl;
+
+    Vector <int> u(n); // создал класс-вектор. С целочисленными координатами
+    for (int i = 0; i < n; i++) // вводим n-чисел
+    {
+        cin >> m; // каждый раз вводим новое число
+        u.Set_coords(m, i); // Функция устанавливает m на i-тую позицию
+    }
+
+    double b1 = u.Get_size(); // получает длину вектора. 
+    cout << "Длина вектора = " << b1 << endl;
+    //
+    //
+    //
+    cout << "Демонстрация работы с типом double" << endl;
+    cout << "Введите размерность вещественнозначного вектора" << endl; 
+    cin >> n;
+    cout << "Введите координаты вектора" << endl;
+
+    Vector <double> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> d;
+        a.Set_coords(d, i);
+    }
+
+    double b = a.Get_size();
+    cout << "Длина вектора = " << b << endl;
+    //
+    //
+    //
+    cout << "Демонстрация работы с типом complex" << endl;
+    cout << "Введите размерность вектора" << endl;
+    cin >> m;
+    Vector <Complex> f(m); // создается вектор, у которого все координаты все комплексные числа
+    cout << "Введите координаты вектора" << endl;
+    Complex z;
+    int r, t;
+
+    for (int i = 0; i < m; i++)
+    {
+        cout << "Введите 2 целых числа для координаты " << i << endl;
+        cin >> r >> t;
+
+        z.set_(r, t); // сформировалось комплексное число 
+        f.Set_coords(z,i); // сформированное комплексное число кладём в определённую позицию
+    }
+
+    double e = f.Get_size();
+    cout << "Вывод комплекснозначного вектора" << endl;
+    for (int i = 0; i < m; i++)
+        cout << f.Get_coords(i) << " ";
+    cout << "Длина вектора = " << e << endl;
+    
+    return 0;
+}
